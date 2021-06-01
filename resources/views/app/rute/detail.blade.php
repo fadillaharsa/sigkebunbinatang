3e39014562a535674f2aa307713a44f5578ac63c
arsabandung <arsabandung@gmail.com>
2021-06-01 20:26:25 +0700

    icon direction edit

--- resources/views/app/rute/detail.blade.php
+++ resources/views/app/rute/detail.blade.php
@@ -40,7 +40,7 @@
 @push('script')
     <script>
         window.action = "direction"
-        window.facilityIcon = "{{$fasilitas->icon}}"
+        window.facilityIcon = "{{$destinasi->facility->icon}}"
     </script>
     <script>
         window.hereApiKey = "{{env('HERE_API_KEY')}}"
